da044674a41aec5004cbbe4de81e73fc10903025
syr <user@syrflover.co>
2024-03-11 20:33:36 +0900

    release

--- encoder/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    #[cfg(not(debug_assertions))]
-    {
-        std::fs::write("./version", env!("CARGO_PKG_VERSION")).unwrap();
-    }
-}
--- encoder/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod config;
-mod error;
-pub mod ffmpeg;
-pub mod ffprobe;
-pub mod time;
-
-pub use error::Error;
-
-pub type Result<T> = std::result::Result<T, error::Error>;
