17075b2864a2a2ce6833ba06d15220bb3539b3ca
syr <user@syrflover.co>
2024-03-11 20:16:32 +0900

    add release.sh

---
+++ release.sh
@@ -0,0 +1,84 @@
+S=$(git status -s)
+
+if ! [ -z "$S" ]; then
+    echo "please commit and push"
+    exit 1
+fi
+
+git checkout gh-pages
+
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+mkdir -p dist
+
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+cd dist
+
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+helm package ../chart
+
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+helm repo index .
+
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+cd ..
+
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+rm -rf `ls | grep -v dist`
+
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+# mv ./dist/* .
+
+# if [ $? -ne 0 ]; then
+#     exit $?
+# fi
+
+git add .
+
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+git push
+
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+git checkout master
+
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+git reset HEAD^
+
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+git checkout .
